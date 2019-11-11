87e8780c80b4f384c0be7b198ed1f2ce5d262e9f
Debony SCHMIDT <debony_13_@hotmail.com>
2019-11-11 11:06:33 +1100

    New Student Migration

--- src/routes.js
+++ src/routes.js
@@ -6,9 +6,9 @@ const routes = new Router();
 routes.get('/', async (req,res) => {
   const student = await Student.create({
     name: 'Debony Schmidt',
-    age: '30',
-    weight: '65',
-    height: '1.64',
+    age: 30,
+    weight: 65,
+    height: 1.64,
   });
 
   return res.json(student);
